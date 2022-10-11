4b95f922947937b6f6bbd303d2be3de276b70e15
ingstor <ingstor1092@gmail.com>
2022-10-11 13:06:33 +0900

    프로젝트 세팅 추가

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,27 @@
 from flask import Flask
+from flask_cors import CORS
+from dotenv import load_dotenv
+
+from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+
+from .db import db
+from .ma import ma
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={r"*": {"origins": "*"}})
+    load_dotenv(".env", verbose=True)
+    app.config.from_object("config.default")
+    app.config.from_envvar("APPLICATION_SETTINGS")
+
+    api = Api(app)
+    jwt = JWTManager(app)
+    migrate = Migrate(app, db)
+
+    db.init_app(app)
+    ma.init_app(app)
+    migrate.init_app(app, db)
     return app
---
+++ config/default.py
@@ -0,0 +1,16 @@
+from datetime import timedelta
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(os.path.join(BASE_DIR, "moneyforrabit.db"))
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+PROPGATE_EXECPTIONS = True
+JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
+SECRET_KEY = os.environ["APP_SECRET_KEY"]
+JWT_BLACKLIST_ENABLED = True
+JWT_BLACKLIST_TOKEN_CHECKS = ["access", "refresh"]
+JSON_AS_ASCII = False
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
+JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
+RESTFUL_JSON = dict(ensure_ascii=False)
