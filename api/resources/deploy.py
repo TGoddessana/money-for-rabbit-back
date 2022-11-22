78221c6fc92605e6a7209d9c4bbd46f1b395059e
ingstor <ingstor1092@gmail.com>
2022-11-22 10:27:05 +0900

    배포 설정

--- api/resources/deploy.py
+++ api/resources/deploy.py
@@ -5,7 +5,7 @@ import os, git
 class DeployServer(Resource):
     def post(self):
         BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-        repo = git.Repo(BASE_DIR)
+        repo = git.Repo(BASE_DIR, search_parent_directories=True)
         origin = repo.remotes.origin
         origin.pull()
         return "Pythonanywhere 서버에 성공적으로 업로드되었습니다!", 200
