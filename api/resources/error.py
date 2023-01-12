cad3b4a4b61781e6973930a097630541d051e488
tgoddessana <twicegoddessana1229@gmail.com>
2023-01-12 20:00:30 +0900

    필요없는 괄호 제

--- api/resources/error.py
+++ api/resources/error.py
@@ -11,11 +11,11 @@ with current_app.app_context():
 
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):
-        return (jsonify({"error": "토큰이 만료되었습니다."}), 401)
+        return jsonify({"error": "토큰이 만료되었습니다."}), 401
 
     @jwt.invalid_token_loader
     def invalid_token_callback(error):
-        return (jsonify({"error": "잘못된 토큰입니다."}), 401)
+        return jsonify({"error": "잘못된 토큰입니다."}), 401
 
     @jwt.unauthorized_loader
     def missing_token_callback(error):
