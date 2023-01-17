41fb52d5e50a4ead5ec4c363545028968a255d01
tgoddessana <twicegoddessana1229@gmail.com>
2023-01-18 00:43:48 +0900

    서비스 로직 분리

--- api/resources/message.py
+++ api/resources/message.py
@@ -1,15 +1,6 @@
-from flask import request, url_for
-from flask_jwt_extended import get_jwt_identity, jwt_required
+from flask_jwt_extended import jwt_required
 from flask_restful import Resource
-from marshmallow import ValidationError
-
-from api.models.message import MessageModel
-from api.models.user import UserModel
-from api.schemas.message import MessageSchema
-from api.utils.response import INTERNAL_SERVER_ERROR, NOT_FOUND, get_response
-
-message_detail_schema = MessageSchema()
-message_list_schema = MessageSchema(many=True)
+from api.services.message import MessageService
 
 
 class MessageDetail(Resource):
@@ -18,14 +9,7 @@ class MessageDetail(Resource):
         """
         쪽지를 특정한 다음, 해당 쪽지의 상세내용을 조회
         """
-        user = UserModel.find_by_id(user_id)
-        if not user:
-            return get_response(False, NOT_FOUND.format("사용자"), 404)
-        message = MessageModel.find_by_id(message_id)
-        if message:
-            return message_detail_schema.dump(message), 200
-        else:
-            return get_response(False, NOT_FOUND.format("쪽지"), 404)
+        return MessageService().detail_view(user_id=user_id, message_id=message_id)
 
 
 class MessageList(Resource):
@@ -34,42 +18,8 @@ class MessageList(Resource):
     def get(cls, user_id):
         """
         유저를 특정한 다음, 해당 유저가 가지고 있는 모든 쪽지들의 목록을 조회
-        쪽지는 요청 보낸 본인만 확인할 수 있음
         """
-        user = UserModel.find_by_id(user_id)
-        if user:
-            if not user.id == get_jwt_identity():
-                return get_response(False, "쪽지는 본인만 조회할 수 있습니다", 403)
-            messages = user.message_set
-            page = request.args.get("page", type=int, default=1)
-            orderd_messages = messages.order_by(MessageModel.id.desc())
-            pagination = orderd_messages.paginate(page, per_page=6, error_out=False)
-
-            next = (
-                f"{request.base_url}?page={pagination.next_num}"
-                if pagination.next_num
-                else None
-            )
-            prev = (
-                f"{request.base_url}?page={pagination.prev_num}"
-                if pagination.prev_num
-                else None
-            )
-
-            return {
-                "user_info": {
-                    "username": user.username,
-                    "email": user.email,
-                    "total_amount": user.total_amount,
-                },
-                "message_set_count": user.message_set_count,
-                "next": next,
-                "prev": prev,
-                "messages": message_list_schema.dump(pagination.items),
-            }
-
-        else:
-            return get_response(False, NOT_FOUND.format("사용자"), 400)
+        return MessageService().list_view(user_id=user_id)
 
     @classmethod
     @jwt_required()
@@ -77,21 +27,4 @@ class MessageList(Resource):
         """
         특정 유저에게 새로운 쪽지를 생성
         """
-        message_json = request.get_json()
-        user = UserModel.find_by_id(get_jwt_identity())
-        if UserModel.find_by_id(user_id):
-            try:
-                new_message = message_detail_schema.load(message_json)
-                new_message.user_id = user_id
-                new_message.author_id = user.id
-            except ValidationError as err:
-                return err.messages, 400
-            except ValueError as err:
-                return get_response(False, str(err), 400)
-            try:
-                new_message.save_to_db()
-            except:
-                return get_response(False, INTERNAL_SERVER_ERROR, 500)
-            return message_detail_schema.dump(new_message), 201
-        else:
-            return get_response(False, NOT_FOUND.format("사용자"), 400)
+        return MessageService().write(user_id=user_id)
---
+++ api/services/message.py
@@ -0,0 +1,74 @@
+from flask import request
+from flask_jwt_extended import get_jwt_identity
+from marshmallow import ValidationError
+
+from api import UserModel, MessageModel
+from api.utils.response import get_response, NOT_FOUND, INTERNAL_SERVER_ERROR
+
+from api.schemas.message import MessageSchema
+
+
+class MessageService:
+    """
+    쪽지
+        - 작성
+        - 상세 조회
+        - 목록 조회
+    """
+
+    def detail_view(self, user_id, message_id):
+        user = UserModel.find_by_id(user_id)
+        message = MessageModel.find_by_id(message_id)
+        if not user:
+            return get_response(False, NOT_FOUND.format("사용자"), 404)
+        if message:
+            return MessageSchema().dump(message), 200
+        return get_response(False, NOT_FOUND.format("쪽지"), 404)
+
+    def list_view(self, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return get_response(False, NOT_FOUND.format("사용자"), 400)
+        if not user.id == get_jwt_identity():
+            return get_response(False, "쪽지는 본인만 조회할 수 있습니다", 403)
+        paginated_posts = user.message_set.order_by(MessageModel.id.desc()).paginate(
+            page=request.args.get("page", type=int, default=1),
+            per_page=6,
+            error_out=False,
+        )
+        next_page = (
+            f"{request.base_url}?page={paginated_posts.next_num}"
+            if paginated_posts.next_num
+            else None
+        )
+        prev_page = (
+            f"{request.base_url}?page={paginated_posts.prev_num}"
+            if paginated_posts.prev_num
+            else None
+        )
+        return {
+            "user_info": {
+                "username": user.username,
+                "email": user.email,
+                "total_amount": user.total_amount,
+            },
+            "message_set_count": user.message_set_count,
+            "next": next_page,
+            "prev": prev_page,
+            "messages": MessageSchema(many=True).dump(paginated_posts.items),
+        }
+
+    def write(self, user_id):
+        message_json = request.get_json()
+        author = UserModel.find_by_id(get_jwt_identity())
+        reader = UserModel.find_by_id(user_id)
+        if not reader:
+            return get_response(False, NOT_FOUND.format("사용자"), 400)
+        validate_result = MessageSchema().validate(message_json)
+        if validate_result:
+            return get_response(False, validate_result, 400)
+        new_message = MessageSchema().load(message_json)
+        new_message.user_id = user_id
+        new_message.author_id = author.id
+        new_message.save_to_db()
+        return MessageSchema().dump(new_message), 201
