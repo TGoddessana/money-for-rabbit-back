7df8092b45b5580999df7306ce7f31633a9ec999
tgoddessana <twicegoddessana1229@gmail.com>
2023-01-19 09:27:45 +0900

    22일 전에는 쪽지 확인 못 하도록 수정

--- api/services/message.py
+++ api/services/message.py
@@ -1,8 +1,8 @@
 from flask import request
 from flask_jwt_extended import get_jwt_identity
-from marshmallow import ValidationError
 
 from api import UserModel, MessageModel
+from api.utils.korean_datetime import get_korean_datetime, MESSAGE_OPEN_DATETIME
 from api.utils.response import get_response, NOT_FOUND, INTERNAL_SERVER_ERROR
 
 from api.schemas.message import MessageSchema
@@ -17,46 +17,52 @@ class MessageService:
     """
 
     def detail_view(self, user_id, message_id):
-        user = UserModel.find_by_id(user_id)
-        message = MessageModel.find_by_id(message_id)
-        if not user:
-            return get_response(False, NOT_FOUND.format("사용자"), 404)
-        if message:
-            return MessageSchema().dump(message), 200
-        return get_response(False, NOT_FOUND.format("쪽지"), 404)
+        if get_korean_datetime() > MESSAGE_OPEN_DATETIME:
+            user = UserModel.find_by_id(user_id)
+            message = MessageModel.find_by_id(message_id)
+            if not user:
+                return get_response(False, NOT_FOUND.format("사용자"), 404)
+            if message:
+                return MessageSchema().dump(message), 200
+            return get_response(False, NOT_FOUND.format("쪽지"), 404)
+        return get_response(False, "쪽지는 22일 이후에만 조회할 수 있습니다.", 400)
 
     def list_view(self, user_id):
-        user = UserModel.find_by_id(user_id)
-        if not user:
-            return get_response(False, NOT_FOUND.format("사용자"), 400)
-        if not user.id == get_jwt_identity():
-            return get_response(False, "쪽지는 본인만 조회할 수 있습니다", 403)
-        paginated_posts = user.message_set.order_by(MessageModel.id.desc()).paginate(
-            page=request.args.get("page", type=int, default=1),
-            per_page=6,
-            error_out=False,
-        )
-        next_page = (
-            f"{request.base_url}?page={paginated_posts.next_num}"
-            if paginated_posts.next_num
-            else None
-        )
-        prev_page = (
-            f"{request.base_url}?page={paginated_posts.prev_num}"
-            if paginated_posts.prev_num
-            else None
-        )
-        return {
-            "user_info": {
-                "username": user.username,
-                "email": user.email,
-                "total_amount": user.total_amount,
-            },
-            "message_set_count": user.message_set_count,
-            "next": next_page,
-            "prev": prev_page,
-            "messages": MessageSchema(many=True).dump(paginated_posts.items),
-        }
+        if get_korean_datetime() > MESSAGE_OPEN_DATETIME:
+            user = UserModel.find_by_id(user_id)
+            if not user:
+                return get_response(False, NOT_FOUND.format("사용자"), 400)
+            if not user.id == get_jwt_identity():
+                return get_response(False, "쪽지는 본인만 조회할 수 있습니다", 403)
+            paginated_posts = user.message_set.order_by(
+                MessageModel.id.desc()
+            ).paginate(
+                page=request.args.get("page", type=int, default=1),
+                per_page=6,
+                error_out=False,
+            )
+            next_page = (
+                f"{request.base_url}?page={paginated_posts.next_num}"
+                if paginated_posts.next_num
+                else None
+            )
+            prev_page = (
+                f"{request.base_url}?page={paginated_posts.prev_num}"
+                if paginated_posts.prev_num
+                else None
+            )
+            return {
+                "user_info": {
+                    "username": user.username,
+                    "email": user.email,
+                    "total_amount": user.total_amount,
+                },
+                "message_set_count": user.message_set_count,
+                "next": next_page,
+                "prev": prev_page,
+                "messages": MessageSchema(many=True).dump(paginated_posts.items),
+            }
+        return get_response(False, "쪽지는 22일 이후에만 조회할 수 있습니다.", 400)
 
     def write(self, user_id):
         message_json = request.get_json()
---
+++ api/utils/korean_datetime.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+import pytz
+
+MESSAGE_OPEN_DATETIME = datetime(2023, 1, 22, tzinfo=pytz.timezone("Asia/Seoul"))
+
+
+def get_korean_datetime():
+    return datetime.now(pytz.timezone("Asia/Seoul"))
