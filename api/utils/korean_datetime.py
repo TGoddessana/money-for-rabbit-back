745f702cc2ceb872585cbffedea3536f0d8feb9d
tgoddessana <twicegoddessana1229@gmail.com>
2023-01-20 22:32:56 +0900

    읽기 날짜 22일로 변경

--- api/utils/korean_datetime.py
+++ api/utils/korean_datetime.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 import pytz
 
-MESSAGE_OPEN_DATETIME = datetime(2023, 1, 19, tzinfo=pytz.timezone("Asia/Seoul"))
+MESSAGE_OPEN_DATETIME = datetime(2023, 1, 22, tzinfo=pytz.timezone("Asia/Seoul"))
 
 
 def get_korean_datetime():
